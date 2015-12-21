d9ac8dc9d3f92ed726557079585ac0702da9b7ca
陈承星 <ccxsungodzzx@gmail.com>
2015-12-21 20:24:04 +0800

    temp fix auth ticket problem

--- auth.js
+++ auth.js
@@ -45,7 +45,8 @@ module.exports = function login(config, jar, cb) {
     }, function(err, httpResponse, body) {
       // request the logined uuap again, and let it redirect for us
       // erp feapps need addition routing policy...
-      var toUrl = TARGET_SERVER + (config.bprouting || + '')
+      // var toUrl = TARGET_SERVER + (config.bprouting || + '')
+      var toUrl = UUAP_SERVER + '/login?service=' + encodeURIComponent(TARGET_SERVER)
       request({
         url: toUrl,
         jar: jar
