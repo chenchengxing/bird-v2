3cce08caacc9560715451a7f8aa5261fbe95f4f2
陈承星 <ccxsungodzzx@gmail.com>
2016-01-28 11:19:29 +0800

    bird

--- birdfile.js
+++ birdfile.js
@@ -1,13 +1,23 @@
+// module.exports = {
+//   name: 'expense',
+//   bird_port: 7000,
+//   // host: '10.42.6.178',
+//   staticFileRootDirPath: '/Users/ccx/workspace/erp-feapps',
+//   server: 'https://erp8040.baidu.com/',
+//   uuap_server: 'http://172.22.1.182:8512/',
+//   username: 'gongxuefeng',
+//   password_suffix: 'qaqa',
+//   bprouting: 'bprouting/BpSecurityAuthentification?bpservice=https%3A%2F%2Ferp8040.baidu.com%2Fbprouting%2FBpFlowRouting%3Fappindex%3Dtrue%26t%3D0.03789611789397895'
+// };
+
+
 module.exports = {
   name: 'expense',
   bird_port: 7000,
-  // host: '10.42.6.178',
   staticFileRootDirPath: '/Users/ccx/workspace/erp-feapps',
   server: 'https://erp8040.baidu.com/',
   uuap_server: 'http://bb-art-stresstest05.bb01.baidu.com:8512/',
   username: 'tujie',
   password_suffix: 'qaqa',
-  bprouting: 'bprouting/BpSecurityAuthentification?bpservice=https%3A%2F%2Ferp8040.baidu.com%2Fbprouting%2FBpFlowRouting%3Fappindex%3Dtrue%26t%3D0.6100948506500572'
-};
-
-
+  bprouting: 'bprouting/BpSecurityAuthentification?bpservice=https%3A%2F%2Ferp8040.baidu.com%2Fbprouting%2FBpFlowRouting%3Fappindex%3Dtrue'
+}
