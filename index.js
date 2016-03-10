654a9a24d0633ed8bdb1f88ee3ae4f8416eb240c
陈承星 <ccxsungodzzx@gmail.com>
2016-03-10 14:46:16 +0800

    make index neat

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ var express = require('express')
 var request = require('request')
 var colors = require('colors');
 
-
 /**
  * start bird with config
  * @param  {Object} config bird-configuration
@@ -17,31 +16,22 @@ module.exports = function start(config) {
     return;
   }
 
-  if (!config || !config.staticFileRootDirPath || !config.server || !config.username) {
+  var jar = request.jar();  // jar to store cookies
+  config = configResolver(config, jar);
+
+  // check if config ok
+  if (!config.ifOk) {
     console.info('check your configuration, pls')
     return;
   }
-  /* silence when not debugging */
+  // silence when not debugging 
   if (!config.debug) {
     global.console.log = function (){};
   }
 
-  var jar = request.jar();  // jar to store cookies
-  config = configResolver(config, jar);
-  var auth = config.auth;
   if (config.middleware) {
-    auth(config, jar);
-    function listAll(list) {
-      return function (req, res, next) {
-        (function iter(i) {
-          var mid = list[i]
-          if (!mid) return next()
-          mid(req, res, function (err) {
-            if (err) return next(err)
-            iter(i + 1)
-          })
-        }(0))
-      }
+    if (config.ifAuth) {
+      auth(config, jar);
     }
     // http://stackoverflow.com/questions/20274483/how-do-i-combine-connect-middleware-into-one-middleware
     return listAll([
@@ -50,39 +40,66 @@ module.exports = function start(config) {
       require('./lib/proxy')(config)
     ]);
   } else {
-    auth(config, jar).then(function () {
-      // setup bird app
+    if (config.ifAuth) {
+      config.auth(config, jar).then(function () {
+        // setup bird app
+        var app = new express()
+        app.all('*', require('./lib/mock')(config))
+        app.all('*', require('./lib/static')(config))
+        app.all('*', require('./lib/change-user')(config))
+        if (config.ifProxy) {
+          app.all('*', require('./lib/proxy')(config))
+        }
+        // go!
+        app.listen(config.birdPort)
+        console.info('BIRD'.rainbow, '============', config.name || '', 'RUNNING at', 'http://localhost:' + config.birdPort, '===============', 'BIRD'.rainbow);
+      })
+    } else {
       var app = new express()
       app.all('*', require('./lib/mock')(config))
       app.all('*', require('./lib/static')(config))
-      app.all('*', require('./lib/change-user')(config))
-      app.all('*', require('./lib/proxy')(config))
-      // go!
-      app.listen(config.bird_port)
-      console.info('BIRD'.rainbow, '============', config.name, 'RUNNING at', 'http://localhost:' + config.bird_port, '===============', 'BIRD'.rainbow);
-    })
+      if (config.ifProxy) {
+        app.all('*', require('./lib/proxy')(config))
+      }
+      app.listen(config.birdPort)
+      console.info('BIRD'.rainbow, '============', config.name || '', 'RUNNING at', 'http://localhost:' + config.birdPort, '===============', 'BIRD'.rainbow);
+    }
   }
-
-  
-  
-  
 }
 
 function configResolver (originConfig, jar) {
-  // if (!originConfig || typeof originConfig !== 'object') {
-  //   return {
-  //     ok: false
-  //   }
-  // }
+  if (!originConfig || typeof originConfig !== 'object') {
+    return {
+      ifOk: false
+    }
+  }
   var config = Object.assign({}, originConfig);
-  // if (!config.staticFileRootDirPath) {
-  //   config.ok = false;
-  //   return config
-  // }
-  // if ()
-  config.auth = require('./auths/' + (originConfig.authType || originConfig.auth_standalone || 'uuap'));
-  config.birdPort = originConfig.bird_port;
+  if (!config.staticFileRootDirPath) {
+    config.ifOk = false;
+    return config
+  }
+  config.ifOk = true;
+  if (config.server) {
+    config.ifProxy = true;
+  }
+  if (config.username && (config.hasOwnProperty('password') || config.hasOwnProperty('password_suffix'))) {
+    config.ifAuth = true;
+    config.auth = require('./auths/' + (originConfig.authType || originConfig.auth_standalone || 'uuap'));
+  }
+  config.birdPort = originConfig.bird_port || 8888;
   config.jar = jar;
   return config;
 }
 
+function listAll(list) {
+  return function (req, res, next) {
+    (function iter(i) {
+      var mid = list[i]
+      if (!mid) return next()
+      mid(req, res, function (err) {
+        if (err) return next(err)
+        iter(i + 1)
+      })
+    }(0))
+  }
+}
